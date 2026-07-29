e7d75dcb75a6fb1b9a81a410b2cd5096a9e66d54
Sr-agente208 <sr.agente208@gmail.com>
2026-07-29 11:55:32 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -344,3 +344,57 @@ window.location.href="login.html";
 
 
 }
+
+function criarConta(){
+
+
+let nome = document.getElementById("nome").value;
+
+let email = document.getElementById("email").value;
+
+let senha = document.getElementById("senha").value;
+
+
+
+if(nome === "" || email === "" || senha === ""){
+
+alert("Preencha todos os campos!");
+
+return;
+
+}
+
+
+
+
+let usuario = {
+
+nome:nome,
+
+email:email,
+
+senha:senha
+
+};
+
+
+
+
+localStorage.setItem(
+
+"usuario",
+
+JSON.stringify(usuario)
+
+);
+
+
+
+alert("✅ Conta criada com sucesso!");
+
+
+
+window.location.href="login.html";
+
+
+}
