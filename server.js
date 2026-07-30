2e621d9cd882fa32ad77be793314354cbff14665
Sr-agente208 <sr.agente208@gmail.com>
2026-07-30 09:23:29 -0300

    geg

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 // =============================
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 
