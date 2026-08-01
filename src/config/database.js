6563b7e6de7eec55424e213cfe2282eae5e412c3
Sr-agente208 <sr.agente208@gmail.com>
2026-07-31 23:52:53 -0300

    refactor: reorganize project structure for better maintainability

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 10000,
+    idleTimeoutMillis: 30000
+});
+
+module.exports = pool;
